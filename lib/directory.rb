f215f93eb44a8afc70bd9fce5ef6d0450dd4afac
thejennywang <thejennywang@gmail.com>
2014-07-02 19:03:58 +0100

    Jenny & Hannah | EOD commit with adding students to csv

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,7 +1,25 @@
+require 'CSV'
+
 def students
 	@students ||= []
 end
 
-def add_student(name, cohort)
-	students << {name: name, cohort: cohort} 
+def student_info(name, cohort)
+	student = {name: name, cohort: cohort} 
+end
+
+def save(student)
+	students << student
+end
+
+def students_list(student)
+	student.values
+end
+
+def students_to_csv(students)
+	CSV.open("./students.csv", "wb") do |csv|
+		students.each do |student|
+      csv << students_list(student)
+    end
+	end
 end
--- spec/directory_spec.rb
+++ spec/directory_spec.rb
@@ -1,25 +1,39 @@
-require 'directory'
+require 'directory'  # ~> LoadError: cannot load such file -- directory
 
 describe 'Student Directory' do
-	
+	xit 'starts with no students' do
+		expect(students).to be_empty
+	end	
+
 	context 'adds students to the directory' do
 
-		it 'starts with no students' do
-			expect(students).to be_empty
-		end
-
 		it 'adds a student' do
-			expect(add_student("name", "cohort")).to eq students
+			student = {name: "Jenny", cohort: "June"}
+			save(student)
+			expect(students[0][:name]).to eq "Jenny" 
 		end
 
 		it 'adds multiple students' do
-			name1 = "Jenny"
-			cohort1 = "June"
-			name2 = "Hannah"
-			cohort2 = "June"
-			add_student(name1, cohort1)
-			add_student(name2, cohort2)
+			student = {name: "Jenny", cohort: "June"}
+			student2 = {name: "Hannah", cohort: "June"}
+			save(student)
+			save(student2)
 			expect((students.size)>1).to be true
+		end
+
+		it 'save students to a csv (with stubbing)' do 
+      # 1. what's the method call
+      # 2. what's the argument(s) if any?
+      # 3. what's the return if any
+      # 4. what's the sequence of methods that needs to be called
+			student = {name: "Jenny", cohort: "June"}
+			students = [student]
+			csv = double
+			expect(csv).to receive(:<<).with(students_list(student))
+			# expect(csv).to receive(:<<).with(students[1])
+			expect(CSV).to receive(:open).with('./students.csv', 'wb').and_yield(csv)
+
+      students_to_csv(students)
 		end
 	end
 end
